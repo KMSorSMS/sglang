15b68af531e856abb9b3c9cf7820f6df8fcce4d8
xiaoqi.31 <xiaoqi.31@jd.com>
2026-07-14 15:30:42 +0800

    cherry-pick: fix: allow DSV4 inference metadata buffer updates

--- python/sglang/srt/layers/attention/deepseek_v4_backend.py
+++ python/sglang/srt/layers/attention/deepseek_v4_backend.py
@@ -1102,7 +1102,8 @@ class DeepseekV4AttnBackend(
                     raw_metadata=self.forward_metadata,
                     online_c128_state_slot_offset=online_c128_state_slot_offset,
                 )
-                full_buf.copy_(temp)
+                with torch.inference_mode():
+                    full_buf.copy_(temp)
                 self.forward_metadata = full_buf
             else:
                 self.forward_metadata = self.make_forward_metadata_from_raw_verify(
@@ -1115,7 +1116,8 @@ class DeepseekV4AttnBackend(
                 temp = self.make_forward_metadata_from_raw_decode(
                     raw_metadata=self.forward_metadata,
                 )
-                full_buf.copy_(temp)
+                with torch.inference_mode():
+                    full_buf.copy_(temp)
                 self.forward_metadata = full_buf
             else:
                 self.forward_metadata = self.make_forward_metadata_from_raw_decode(
